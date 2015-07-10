99b864dd8d850588f7b704a05f150efc5a077c0a
AgeOf <tylerecouture@gmail.com>
2015-07-10 01:42:12 -0700

    Quest model prerequisites added

--- src/quest_manager/admin.py
+++ src/quest_manager/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-from .models import Quest
+from .models import Quest, Category, Prerequisite
+
+class PrerequisiteInLine(admin.StackedInline):
+    model = Prerequisite
+    fk_name = "parent_quest"
 
 class QuestAdmin(SummernoteModelAdmin):
-    None
+    list_display = ('name', 'xp','visible_to_students','max_repeats','date_expired')
+    list_filter = ['visible_to_students','max_repeats','verification_required']
+    search_fields = ['name']
+    inlines = [
+        PrerequisiteInLine,
+    ]
 
 admin.site.register(Quest, QuestAdmin)
+admin.site.register(Category)
+admin.site.register(Prerequisite)
