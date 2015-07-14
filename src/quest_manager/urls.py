b6fc67975f117ea51e889a253220448485840a0b
Tylere <tylerecouture@gmail.com>
2015-07-14 00:02:07 -0700

    add files, still fixing git screw ups

---
+++ src/quest_manager/urls.py
@@ -0,0 +1,28 @@
+"""quest_manager URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import  url
+from quest_manager import views
+
+# Admin site customizations
+
+urlpatterns = [
+    url(r'^$', views.quests, name='quests'),
+    url(r'^email_demo/$', views.email_demo, name='email_demo'),
+    # url(r'^new_quest_custom/$', views.new_quest_custom, name='new_quest_custom'),
+    # url(r'^quests/$', 'quest_manager.views.quests', name='quests'),
+     # ex: /quest/25/
+    url(r'^(?P<quest_id>[0-9]+)/$', views.detail, name='detail'),
+]
