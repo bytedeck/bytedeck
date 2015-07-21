30144b2f64f0549a00adc815a87b7be597d5f0b6
AgeOf <tylerecouture@gmail.com>
2015-07-21 11:50:45 -0700

    formatting of sidebar, navbar, @media queries

--- src/announcements/urls.py
+++ src/announcements/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import  url
 from announcements import views
 
 urlpatterns = [
-    url(r'^$', views.List.as_view(), name='profile_list'),
+    url(r'^$', views.List.as_view(), name='list'),
     # url(r'^create/$', views.ProfileCreate.as_view(), name='profile_create'),
     # url(r'^(?P<pk>\d+)/$', views.ProfileDetail.as_view(), name='profile_detail'),
     # url(r'^(?P<pk>\d+)/edit/$', views.ProfileUpdate.as_view(), name='profile_update'),
