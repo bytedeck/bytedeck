05c5d58f10dca2768990180933999481ba275639
AgeOf <tylerecouture@gmail.com>
2015-07-25 17:13:38 -0700

    As UserCourse model to profile_manager

--- src/profile_manager/admin.py
+++ src/profile_manager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, UserCourse
 # Register your models here.
 
 admin.site.register(Profile)
+
+admin.site.register(UserCourse)
--- src/profile_manager/models.py
+++ src/profile_manager/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-import datetime
-YEAR_CHOICES = []
-for r in range(datetime.datetime.now().year, datetime.datetime.now().year+4):
-    YEAR_CHOICES.append((r,r)) #(actual value, human readable name) tuples
+from quest_manager.models import Course
+
+from datetime import datetime
+
+GRAD_YEAR_CHOICES = []
+for r in range(datetime.now().year, datetime.now().year+4):
+        GRAD_YEAR_CHOICES.append((r,r)) #(actual value, human readable name) tuples
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="profile_user", null=False)
@@ -15,13 +18,41 @@ class Profile(models.Model):
     preferred_name = models.CharField(max_length=50, null=True, blank=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     student_number = models.PositiveIntegerField(unique=True, blank=False, null=False)
-    grad_year = models.PositiveIntegerField(choices=YEAR_CHOICES)
+    grad_year = models.PositiveIntegerField(choices=GRAD_YEAR_CHOICES)
     datetime_created = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-    
 
-    def get_absolute_url(self):
-        return reverse('profile_detail', kwargs={'pk': self.pk})
 
-    def __str__(self):
-        return str(self.user)
+class UserCourse(models.Model):
+
+    SEMESTER_CHOICES = ((1,1),(2,2),)
+    BLOCK_CHOICES = (('A','A'),('B','B'),('C','C'),('D','D'),)
+
+    def get_current_school_year():
+        #if it is currently beyond August, go 2016/17 else got 2015/16
+        year_first = datetime.now()
+        if datetime.now().month > 7 :
+            year_first = year_first.replace(year = year_first.year + 1)
+        year_second = year_first.replace(year = year_first.year + 1)
+        return year_first.strftime("%Y/") + year_second.strftime("%y")
+
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    school_year = models.CharField(max_length = 7, default = get_current_school_year())
+    semester = models.PositiveIntegerField(choices=SEMESTER_CHOICES)
+    block = models.CharField(max_length=1, choices=BLOCK_CHOICES)
+    course_on_timetable = models.ForeignKey(Course, help_text = 'The course your are registered for in your timetable')
+
+
+
+    # alias = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    # first_name = models.CharField(max_length=50, null=False, blank=False)
+    # last_name = models.CharField(max_length=50, null=False, blank=False)
+    # preferred_name = models.CharField(max_length=50, null=True, blank=True)
+    # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    # student_number = models.PositiveIntegerField(unique=True, blank=False, null=False)
+    # grad_year = models.PositiveIntegerField(choices=YEAR_CHOICES)
+    # datetime_created = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    # def __str__(self):
+    #     return str(self.user + ":")
