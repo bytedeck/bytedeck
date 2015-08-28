2fca4192fe48933a7356cae75dd11005a47aade3
AgeOf <tylerecouture@gmail.com>
2015-08-28 11:58:10 -0700

    profile XP bar

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -40,35 +40,4 @@ admin.site.register(Semester, SemesterAdmin)
 admin.site.register(Block)
 admin.site.register(Course)
 admin.site.register(DateType)
-
-
-# class Semester(models.Model):
-#     SEMESTER_CHOICES = ((1,1),(2,2),)
-#
-#     number = models.PositiveIntegerField(choices=SEMESTER_CHOICES)
-#     first_day = models.DateField(blank=True, null=True)
-#     last_day = models.DateField(blank=True, null=True)
-#
-# class ExcludedDate(models.Model):
-#     semester = models.ForeignKey(Semester)
-#     date_type = models.ForeignKey(DateType)
-#     date = models.DateField(unique=True)
-#
-# class DateType(models.Model):
-#     date_type = models.CharField(max_length=50, unique=True)
-#
-# class Block(models.Model):
-#     block = models.CharField(max_length=50, unique=True)
-#
-# class CourseStudent(model.Models):
-#     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-#     semester = models.ForeignKey(Semester)
-#     block = models.ForeignKey(Block)
-#     course = models.ForeignKey(Course)
-#     grade = models.PositiveIntegerField()
-#     active = models.BooleanField(default=True)
-#
-# class Course(models.Model):
-#     title = models.CharField(max_length=50, unique=True)
-#     icon = models.ImageField(upload_to='icons/', null=True, blank=True)
-#     active = models.BooleanField(default=True)
+admin.site.register(Rank)
