d0b8621eff0e283da515f6f937b6e556e878da31
AgeOf <tylerecouture@gmail.com>
2015-09-11 21:49:36 -0700

    redirect staff to approval tab

--- src/hackerspace_online/views.py
+++ src/hackerspace_online/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth.decorators import user_passes_test
 
 from .settings import local
 
-@user_passes_test(lambda user: not user.get_username(), login_url='/quests', redirect_field_name=None)
+# @user_passes_test(lambda user: not user.get_username(), login_url='/quests', redirect_field_name=None)
 def home(request):
-    #
-    # if request.user.is_staff:
-    #     redirect('quests:approvals')
-    #
-    # if request.user.is_authenticated():
-    #     redirect('quests:quests')
+
+    if request.user.is_staff:
+        return redirect('quests:approvals')
+
+    if request.user.is_authenticated():
+        return redirect('quests:quests')
 
     return render(request, "home.html", {})
