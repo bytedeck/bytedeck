826991b03e34aebda51c7a48009f6d9187154c4c
AgeOf <tylerecouture@gmail.com>
2017-01-14 21:24:31 -0800

    autofocus on username input

--- src/hackerspace_online/settings/__init__.py
+++ src/hackerspace_online/settings/__init__.py
@@ -1,6 +1,7 @@
 from .base import *
 
 try:
+    # this file will only exists on the production server
     from .production_hackerspace import *
 except:
     print("***** NO PRODUCTION SETTINGS FOUND     *******")
