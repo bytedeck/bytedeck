20239bd9c6b412001c251e310e82bcb749961493
Nandini Bulusu <nanbul785@gmail.com>
2017-10-25 12:11:53 -0700

    added the icon model

--- src/utilities/models.py
+++ src/utilities/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Icon(models.Model):
+    icon_name = models.CharField(max_length=50)
+    icon_image= models.ImageField(upload_to='icons/', blank=True, null=True)
