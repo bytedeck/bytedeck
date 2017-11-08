70fe8c0a6f94ff1779b44a3dc375123d06c5f11c
tylerecouture <tylerecouture@gmail.com>
2017-11-08 08:24:33 -0800

    announcements admin search

--- src/announcements/admin.py
+++ src/announcements/admin.py
@@ -7,7 +7,9 @@ from .models import Announcement
 
 
 class AnnouncementAdmin(SummernoteModelAdmin):
-    None
+    list_display = ('title', 'datetime_released', )
+    list_filter = ['datetime_released']
+    search_fields = ['title', 'content', ]
 
 
 admin.site.register(Announcement, AnnouncementAdmin)
