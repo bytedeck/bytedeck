b8d50e93a60d65e7c8de8c3bf6f34ceedd136f8b
AgeOf <tylerecouture@gmail.com>
2018-01-07 14:14:15 -0800

    fix jquery plugin pack imgs and figures

---
+++ src/static_in_project/static_root/js/jquery-packed-img-strip.js
@@ -0,0 +1,28 @@
+// https://learn.jquery.com/plugins/basic-plugin-creation/
+
+(function($) {
+  //noinspection JSAnnotator
+  $.fn.pack = function(options) {
+    var settings = $.extend({
+        // margin: 10px;
+      }, options );
+
+    // this should be a container div with a series of img or figure children.
+    $(this).children().each(function() {
+        if ($(this).is("img")) {
+          var img = this;
+          $(this).wrap('<div />');
+          var flexWrapper = $(this).parent();
+        } else { // <figure>
+          var img = $(this).find("img")[0];
+          var flexWrapper = $(this)
+        }
+        let aspect = img.naturalWidth / img.naturalHeight;
+        console.log(flexWrapper);
+        flexWrapper.css({ flex: aspect + "" });
+      });
+
+    return this;
+    // });
+  };
+})(jQuery);
