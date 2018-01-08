79f1a84bb4942fd242094e9362664b6d1d1fea54
AgeOf <tylerecouture@gmail.com>
2018-01-07 21:34:35 -0800

    update jquery-pack

--- src/static_in_project/static_root/js/jquery-packed-img-strip.js
+++ src/static_in_project/static_root/js/jquery-packed-img-strip.js
@@ -13,7 +13,7 @@
           var img = this;
           $(this).wrap('<div />');
           var flexWrapper = $(this).parent();
-        } else { // <figure>
+        } else { // <figure> or <a>
           var img = $(this).find("img")[0];
           var flexWrapper = $(this)
         }
