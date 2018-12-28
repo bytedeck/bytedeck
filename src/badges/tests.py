edd5b5d45bd3014dd9e246691375fc781a835a07
Tylere Couture <tylerecouture@gmail.com>
2018-12-28 13:10:43 -0800

    mucking around with tests and Path and initia data fixture

--- src/badges/tests.py
+++ src/badges/tests.py
@@ -1,1 +1,50 @@
 # Create your tests here.
+from django.http import HttpRequest
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
+
+from badges.urls import urlpatterns
+
+
+class PageTests(TestCase):
+
+    fixtures = ['initial_data.json']
+
+
+    def get_response_code(self, url_pattern_name, args=None):
+        response = self.client.get(reverse(url_pattern_name, args))
+        return response.status_code
+
+    def test_all_badge_page_status_codes(self):
+
+
+
+        # for url in urlpatterns:
+        self.assertEquals(self.get_response_code('badges:list'), 200)
+        self.assertEquals(self.get_response_code('badges:badge_create'), 200)
+        self.assertEquals(self.get_response_code('badges:bulk_grant'), 200)
+        self.assertEquals(self.get_response_code('badges:badge_detail', args=[1]), 200)
+
+
+
+
+        # for url in urlpatterns:
+        #     print(url)
+
+    # def test_view_url_by_name(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEquals(response.status_code, 200)
+    #
+    # def test_view_uses_correct_template(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEquals(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'home.html')
+    #
+    # def test_home_page_contains_correct_html(self):
+    #     response = self.client.get('/')
+    #     self.assertContains(response, '<h1>Homepage</h1>')
+    #
+    # def test_home_page_does_not_contain_incorrect_html(self):
+    #     response = self.client.get('/')
+    #     self.assertNotContains(
+    #         response, 'Hi there! I should not be on the page.')
--- src/badges/urls.py
+++ src/badges/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path
+
 from badges import views
 
 from django.conf.urls import url
@@ -5,16 +7,15 @@ from django.conf.urls import url
 app_name = 'badges'
 
 urlpatterns = [
-    url(r'^$', views.badge_list, name='list'),
-    url(r'^create/$', views.badge_create, name='badge_create'),
-    url(r'^(?P<badge_id>[0-9]+)/$', views.detail, name='badge_detail'),
+    path('', views.badge_list, name='list'),
+    path('create/', views.badge_create, name='badge_create'),
+    path('<int:badge_id>', views.detail, name='badge_detail'),
     url(r'^(?P<pk>[0-9]+)/edit/$', views.BadgeUpdate.as_view(), name='badge_update'),
     url(r'^(?P<badge_id>[0-9]+)/copy/$', views.badge_copy, name='badge_copy'),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.BadgeDelete.as_view(), name='badge_delete'),
     # url(r'^(?P<badge_id>[0-9]+)/grant/(?P<user_id>[0-9]+)/$', views.grant, name='grant'),
     url(r'^(?P<badge_id>[0-9]+)/grant/(?P<user_id>[0-9]+)/$', views.assertion_create, name='grant'),
     url(r'^(?P<badge_id>[0-9]+)/grant/bulk/$', views.bulk_assertion_create, name='bulk_grant_badge'),
-    url(r'^grant/bulk/$', views.bulk_assertion_create, name='bulk_grant'),
+    path('grant/bulk/', views.bulk_assertion_create, name='bulk_grant'),
     url(r'^assertion/(?P<assertion_id>[0-9]+)/revoke/$', views.assertion_delete, name='revoke'),
-
 ]
