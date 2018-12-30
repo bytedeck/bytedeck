8a74ffb690f7956552de7aac8a29f150a635b1e7
Tylere Couture <tylerecouture@gmail.com>
2018-12-30 15:09:55 -0800

    fix bug in tests cause by djconfig

--- src/badges/tests.py
+++ src/badges/tests.py
@@ -1,4 +1,5 @@
 # Create your tests here.
+import djconfig
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -7,8 +8,7 @@ from djconfig.utils import override_djconfig
 from badges.models import BadgeAssertion, Badge
 
 
-@override_djconfig(hs_active_semester="1")
-class BadgeViewTests(TestCase):
+class ViewTests(TestCase):
 
     # includes some basic model data
     fixtures = ['initial_data.json']
@@ -16,16 +16,18 @@ class BadgeViewTests(TestCase):
     def setUp(self):
         User = get_user_model()
         self.test_password = 'password'
-        # need a teacher before students can be created or the profile creationwill fail......why?
+
+        # need a teacher before students can be created or the profile creation will fail......why?
         self.test_teacher = User.objects.create_user('test_teacher', password=self.test_password, is_staff=True)
         self.test_student1 = User.objects.create_user('test_student', password=self.test_password)
         self.test_student2 = User.objects.create_user('test_student2', password=self.test_password)
 
         self.test_badge = Badge.objects.get(pk=1)  # create by fixture
 
-        self.test_assertion = BadgeAssertion.objects.create_assertion(user=self.test_student1,
-                                                                      badge=self.test_badge,
-                                                                      issued_by=self.test_teacher)
+        self.test_assertion = BadgeAssertion.objects.create(user=self.test_student1,
+                                                            badge=self.test_badge,
+                                                            issued_by=self.test_teacher,
+                                                            )
 
     def test_all_badge_page_status_codes_for_anonymous(self):
         # If not logged in then should redirect to home page
@@ -54,7 +56,7 @@ class BadgeViewTests(TestCase):
         self.assertEquals(self.client.get(reverse('badges:bulk_grant')).status_code, 302)
         self.assertEquals(self.client.get(reverse('badges:revoke', args=[s_pk])).status_code, 302)
 
-    def test_all_badge_page_status_codes_for_students(self):
+    def test_all_badge_page_status_codes_for_teachers(self):
         # log in a teacher
         success = self.client.login(username=self.test_teacher.username, password=self.test_password)
         self.assertTrue(success)
