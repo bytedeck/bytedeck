ac4ddb7e13bd67d2150113b41ca0c964d430fa78
Tylere Couture <tylerecouture@gmail.com>
2018-12-30 14:11:22 -0800

    test view status_codes

--- src/badges/tests.py
+++ src/badges/tests.py
@@ -1,35 +1,79 @@
 # Create your tests here.
-from django.http import HttpRequest
-from django.test import SimpleTestCase, TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 from django.urls import reverse
+from djconfig.utils import override_djconfig
 
-from badges.urls import urlpatterns
+from badges.models import BadgeAssertion, Badge
 
 
-class PageTests(TestCase):
+@override_djconfig(hs_active_semester="1")
+class BadgeViewTests(TestCase):
 
+    # includes some basic model data
     fixtures = ['initial_data.json']
 
+    def setUp(self):
+        User = get_user_model()
+        self.test_password = 'password'
+        # need a teacher before students can be created or the profile creationwill fail......why?
+        self.test_teacher = User.objects.create_user('test_teacher', password=self.test_password, is_staff=True)
+        self.test_student1 = User.objects.create_user('test_student', password=self.test_password)
+        self.test_student2 = User.objects.create_user('test_student2', password=self.test_password)
 
-    def get_response_code(self, url_pattern_name, args=None):
-        response = self.client.get(reverse(url_pattern_name, args))
-        return response.status_code
+        self.test_badge = Badge.objects.get(pk=1)  # create by fixture
 
-    def test_all_badge_page_status_codes(self):
+        self.test_assertion = BadgeAssertion.objects.create_assertion(user=self.test_student1,
+                                                                      badge=self.test_badge,
+                                                                      issued_by=self.test_teacher)
 
+    def test_all_badge_page_status_codes_for_anonymous(self):
+        # If not logged in then should redirect to home page
+        self.assertEquals(self.client.get(reverse('badges:list')).status_code, 302)
 
+    def test_all_badge_page_status_codes_for_students(self):
 
-        # for url in urlpatterns:
-        self.assertEquals(self.get_response_code('badges:list'), 200)
-        self.assertEquals(self.get_response_code('badges:badge_create'), 200)
-        self.assertEquals(self.get_response_code('badges:bulk_grant'), 200)
-        self.assertEquals(self.get_response_code('badges:badge_detail', args=[1]), 200)
+        # log in a student
+        success = self.client.login(username=self.test_student1.username, password=self.test_password)
+        self.assertTrue(success)
 
+        b_pk = self.test_badge.pk
+        a_pk = self.test_assertion.pk
+        s_pk = self.test_student1.pk
 
+        self.assertEquals(self.client.get(reverse('badges:list')).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:badge_detail', args=[b_pk])).status_code, 200)
 
+        # students shouldn't have access to these and should be redirected
+        self.assertEquals(self.client.get(reverse('badges:badge_create')).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:badge_update', args=[b_pk])).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:badge_copy', args=[b_pk])).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:badge_delete', args=[b_pk])).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:grant', args=[b_pk, s_pk])).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:bulk_grant_badge', args=[b_pk])).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:bulk_grant')).status_code, 302)
+        self.assertEquals(self.client.get(reverse('badges:revoke', args=[s_pk])).status_code, 302)
 
-        # for url in urlpatterns:
-        #     print(url)
+    def test_all_badge_page_status_codes_for_students(self):
+        # log in a teacher
+        success = self.client.login(username=self.test_teacher.username, password=self.test_password)
+        self.assertTrue(success)
+
+        b_pk = self.test_badge.pk
+        a_pk = self.test_assertion.pk
+        s_pk = self.test_student1.pk
+
+        self.assertEquals(self.client.get(reverse('badges:list')).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:badge_detail', args=[b_pk])).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:badge_create')).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:badge_update', args=[b_pk])).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:badge_copy', args=[b_pk])).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:badge_delete', args=[b_pk])).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:grant', args=[b_pk, s_pk])).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:bulk_grant_badge', args=[b_pk])).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:bulk_grant')).status_code, 200)
+        self.assertEquals(self.client.get(reverse('badges:revoke', args=[a_pk])).status_code, 200)
+
 
     # def test_view_url_by_name(self):
     #     response = self.client.get(reverse('home'))
