568f6261a4dff9f368ea30e9a1c2072e7ce50175
tylerecouture <tylerecouture@gmail.com>
2019-02-13 16:13:28 -0800

    Notification test file skeletons

---
+++ src/notifications/tests/test_models.py
@@ -0,0 +1,104 @@
+from random import randint
+
+import djconfig
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+from model_mommy import mommy
+from model_mommy.recipe import Recipe
+
+from notifications.models import Notification
+
+
+class NotificationTestModel(TestCase):
+
+    def setUp(self):
+        djconfig.reload_maybe()  # https://github.com/nitely/django-djconfig/issues/31#issuecomment-451587942
+
+        User = get_user_model()
+        self.teacher = Recipe(User, is_staff=True).make()  # need a teacher or student creation will fail.
+        self.student = mommy.make(User)
+        self.notification = mommy.make(Notification)
+        # self.assertion = mommy.make(BadgeAssertion)
+        # self.badge = Recipe(Badge, xp=20).make()
+        # self.badge_assertion_recipe = Recipe(BadgeAssertion, user=self.student, badge=self.badge)
+
+    def test_badge_assertion_creation(self):
+        self.assertIsInstance(self.notification, Notification)
+        self.assertIsNotNone(str(self.notification))
+
+        print(str(self.notification))
+    #
+    # def test_badge_assertion_url(self):
+    #     self.assertEquals(self.client.get(self.assertion.get_absolute_url(), follow=True).status_code, 200)
+    #
+    # def test_badge_assertion_count(self):
+    #     num = randint(1, 9)
+    #     for _ in range(num):
+    #         badge_assertion = BadgeAssertion.objects.create_assertion(
+    #             self.student,
+    #             self.badge
+    #         )
+    #         # Why doesn't below work?
+    #         #badge_assertion = self.badge_assertion_recipe.make()
+    #     count = badge_assertion.count()
+    #     # print(num, count)
+    #     self.assertEquals(num, count)
+    #
+    # def test_badge_assertion_count_bootstrap_badge(self):
+    #     """Returns empty string if count < 2, else returns proper count"""
+    #     badge_assertion = mommy.make(BadgeAssertion)
+    #     self.assertEquals(badge_assertion.count_bootstrap_badge(), "")
+    #
+    #     num = randint(1, 9)
+    #     for _ in range(num):
+    #         badge_assertion = BadgeAssertion.objects.create_assertion(
+    #             self.student,
+    #             self.badge
+    #         )
+    #         # Why doesn't below work?
+    #         #badge_assertion = self.badge_assertion_recipe.make()
+    #     count = badge_assertion.count_bootstrap_badge()
+    #     # print(num, count)
+    #     self.assertEquals(num, count)
+    #
+    # def test_badge_assertion_get_duplicate_assertions(self):
+    #     num = randint(1, 9)
+    #     values = []
+    #     for _ in range(num):
+    #         badge_assertion = self.badge_assertion_recipe.make()
+    #         values.append(repr(badge_assertion))
+    #
+    #     qs = badge_assertion.get_duplicate_assertions()
+    #     self.assertQuerysetEqual(list(qs), values,)
+    #
+    # def test_badge_assertion_manager_create_assertion(self):
+    #     new_assertion = BadgeAssertion.objects.create_assertion(
+    #         self.student,
+    #         mommy.make(Badge)
+    #     )
+    #     self.assertIsInstance(new_assertion, BadgeAssertion)
+    #
+    # def test_badge_assertion_manager_xp_to_date(self):
+    #     xp = BadgeAssertion.objects.calculate_xp_to_date(self.student, timezone.now())
+    #     self.assertEqual(xp, 0)
+    #
+    #     # give them a badge assertion and make sure the XP works
+    #     BadgeAssertion.objects.create_assertion(
+    #         self.student,
+    #         self.badge
+    #     )
+    #     xp = BadgeAssertion.objects.calculate_xp_to_date(self.student, timezone.now())
+    #     self.assertEqual(xp, self.badge.xp)
+    #
+    # def test_badge_assertion_manager_get_by_type_for_user(self):
+    #     badge_list_by_type = BadgeAssertion.objects.get_by_type_for_user(self.student)
+    #     self.assertIsInstance(badge_list_by_type, list)
+    #     # TODO need to test this properly
+    #
+    # def test_badge_assertion_manager_check_for_new_assertions(self):
+    #     BadgeAssertion.objects.check_for_new_assertions(self.student)
+    #     # TODO need to test this properly
+
+
+
---
+++ src/notifications/tests/test_views.py
@@ -0,0 +1,113 @@
+# Create your tests here.
+import djconfig
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from model_mommy import mommy
+
+from notifications.models import Notification
+
+
+class NotificationViewTests(TestCase):
+
+    # includes some basic model data
+    # fixtures = ['initial_data.json']
+
+    def setUp(self):
+        djconfig.reload_maybe()  # https://github.com/nitely/django-djconfig/issues/31#issuecomment-451587942
+
+        User = get_user_model()
+
+        # need a teacher and a student with known password so tests can log in as each, or could use force_login()?
+        self.test_password = "password"
+
+        # need a teacher before students can be created or the profile creation will fail when trying to notify
+        self.test_teacher = User.objects.create_user('test_teacher', password=self.test_password, is_staff=True)
+        self.test_student1 = User.objects.create_user('test_student', password=self.test_password)
+        self.test_student2 = mommy.make(User)
+
+        self.test_notification = mommy.make(Notification)
+
+#
+#     def test_all_badge_page_status_codes_for_anonymous(self):
+#         ''' If not logged in then all views should redirect to home page  '''
+#
+#         self.assertRedirects(
+#             response=self.client.get(reverse('badges:list')),
+#             expected_url='%s?next=%s' % (reverse('home'), reverse('badges:list')),
+#         )
+#
+#         # for path in urlpatterns:
+#         #     name = 'badges:%s' % path.name
+#         #     # path.name
+#         #     # print(url)
+#         #     self.assertRedirects(
+#         #         response=self.client.get(reverse(name)),
+#         #         expected_url='%s?next=%s' % (reverse('home'), reverse(name)),
+#         #         msg_prefix=name,
+#         #     )
+#
+#     def test_all_badge_page_status_codes_for_students(self):
+#
+#         # log in a student
+#         success = self.client.login(username=self.test_student1.username, password=self.test_password)
+#         self.assertTrue(success)
+#
+#         b_pk = self.test_badge.pk
+#         a_pk = self.test_assertion.pk
+#         s_pk = self.test_student1.pk
+#
+#         self.assertEquals(self.client.get(reverse('badges:list')).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:badge_detail', args=[b_pk])).status_code, 200)
+#
+#         # students shouldn't have access to these and should be redirected
+#         self.assertEquals(self.client.get(reverse('badges:badge_create')).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:badge_update', args=[b_pk])).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:badge_copy', args=[b_pk])).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:badge_delete', args=[b_pk])).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:grant', args=[b_pk, s_pk])).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:bulk_grant_badge', args=[b_pk])).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:bulk_grant')).status_code, 302)
+#         self.assertEquals(self.client.get(reverse('badges:revoke', args=[s_pk])).status_code, 302)
+#
+#     def test_all_badge_page_status_codes_for_teachers(self):
+#         # log in a teacher
+#         success = self.client.login(username=self.test_teacher.username, password=self.test_password)
+#         self.assertTrue(success)
+#
+#         b_pk = self.test_badge.pk
+#         a_pk = self.test_assertion.pk
+#         s_pk = self.test_student1.pk
+#
+#         self.assertEquals(self.client.get(reverse('badges:list')).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:badge_detail', args=[b_pk])).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:badge_create')).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:badge_update', args=[b_pk])).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:badge_copy', args=[b_pk])).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:badge_delete', args=[b_pk])).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:grant', args=[b_pk, s_pk])).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:bulk_grant_badge', args=[b_pk])).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:bulk_grant')).status_code, 200)
+#         self.assertEquals(self.client.get(reverse('badges:revoke', args=[a_pk])).status_code, 200)
+#
+#
+# # class ViewTests(TestCase):
+#
+#
+#     # def test_view_url_by_name(self):
+#     #     response = self.client.get(reverse('home'))
+#     #     self.assertEquals(response.status_code, 200)
+#     #
+#     # def test_view_uses_correct_template(self):
+#     #     response = self.client.get(reverse('home'))
+#     #     self.assertEquals(response.status_code, 200)
+#     #     self.assertTemplateUsed(response, 'home.html')
+#     #
+#     # def test_home_page_contains_correct_html(self):
+#     #     response = self.client.get('/')
+#     #     self.assertContains(response, '<h1>Homepage</h1>')
+#     #
+#     # def test_home_page_does_not_contain_incorrect_html(self):
+#     #     response = self.client.get('/')
+#     #     self.assertNotContains(
+#     #         response, 'Hi there! I should not be on the page.')
