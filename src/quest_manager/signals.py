a9abbc061ae6d531719b5dc091f8db7565875361
tylerecouture <tylerecouture@gmail.com>
2019-05-02 15:11:00 -0700

    hotfix auto-indentation/formatting of quest text

--- src/quest_manager/signals.py
+++ src/quest_manager/signals.py
@@ -1,8 +1,25 @@
+import re
+
+from bs4 import BeautifulSoup
+from bs4 import DEFAULT_OUTPUT_ENCODING
+from bs4 import Tag
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from tidylib import Tidy
 
 from quest_manager.models import Quest
+
+
+class UglySoup(BeautifulSoup):
+    r = re.compile(r'^(\s*)', re.MULTILINE)
+
+    def insert_before(self, successor):
+        pass
+
+    def insert_after(self, successor):
+        pass
+
+    def prettify_indented(self, encoding=None, formatter="minimal", indent_width=4):
+        return self.r.sub(r'\1' * indent_width, self.prettify(encoding, formatter))
 
 
 @receiver(pre_save, sender=Quest)
@@ -11,12 +28,14 @@ def quest_pre_save_callback(sender, instance, **kwargs):
 
 
 def tidy_html(content):
+    soup = UglySoup(content, "html.parser")
+    return soup.prettify_indented()
 
-    config = {
-        # "break-before-br": True,
-        # "doctype": "omit",
-        # "output-html": True,
-        # "wrap": 0,
-    }
-    result, errors = Tidy().tidy_fragment(content, options=config)
-    return result
+    # config = {
+    #     # "break-before-br": True,
+    #     # "doctype": "omit",
+    #     # "output-html": True,
+    #     # "wrap": 0,
+    # }
+    # result, errors = Tidy().tidy_fragment(content, options=config)
+    # return result
