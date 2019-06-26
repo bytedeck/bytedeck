98640aaa10344ca759fa66e82309a3da824d5e50
tylerecouture <tylerecouture@gmail.com>
2019-06-26 14:35:33 -0700

    typo and redis ip

--- src/hackerspace_online/settings/base.py
+++ src/hackerspace_online/settings/base.py
@@ -159,7 +159,7 @@ TEMPLATES = [
 ]
 
 # Redis:
-REDIS_HOST = os.environ.get('REDIS_HOST', '0.0.0.0')
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
 
 CACHES = {
