89f0a0af67c8487afc34d9c836d2663b73013294
fibio <fibio.tany@gmail.com>
2019-07-02 13:54:56 +0300

    Extended PrereqAllConditionsMet.ids, updated tasks to use transactions

--- src/prerequisites/tasks.py
+++ src/prerequisites/tasks.py
@@ -5,8 +5,9 @@ from functools import wraps
 from django.core.cache import cache
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.db.utils import OperationalError
-from celery import shared_task
+from celery import shared_task, Task
 
 from quest_manager.models import Quest
 from prerequisites.models import Prereq, PrereqAllConditionsMet
@@ -18,24 +19,23 @@ logger = logging.getLogger(__name__)
 User = get_user_model()
 
 
-def try_except_task(func):
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
+class TransactionAwareTask(Task):
+    abstract = True
+
+    def apply_async(self, *args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            transaction.on_commit(lambda: super(TransactionAwareTask, self).apply_async(*args, **kwargs))
         except OperationalError:
-            # logger.error(traceback.format_exc())
-            # countdown = kwargs.get('countdown', 30)
-            # kwargs['countdown'] = countdown * 2
-            return func(*args, **kwargs)
+            logger.info(traceback.format_exc())
+            countdown = kwargs.get('countdown', 60)
+            kwargs['countdown'] = countdown * 2
+            return super(TransactionAwareTask, self).apply_async(*args, **kwargs)
         except Exception:
             logger.error(traceback.format_exc())
-    return decorated_function
 
 
-@shared_task(name='update_quest_conditions_for_user', max_retries=settings.CELERY_TASK_MAX_RETRIES)
-@try_except_task
-def update_quest_conditions_for_user(user_id):
+@shared_task(base=TransactionAwareTask, bind=True, name='update_quest_conditions_for_user', max_retries=settings.CELERY_TASK_MAX_RETRIES)
+def update_quest_conditions_for_user(self, user_id):
     user = User.objects.filter(id=user_id).first()
     if not user:
         return
@@ -45,9 +45,8 @@ def update_quest_conditions_for_user(user_id):
     return met_list.id
 
 
-@shared_task(name='update_conditions_for_quest', max_retries=settings.CELERY_TASK_MAX_RETRIES)
-@try_except_task
-def update_conditions_for_quest(quest_id, start_from_user_id):
+@shared_task(base=TransactionAwareTask, bind=True, name='update_conditions_for_quest', max_retries=settings.CELERY_TASK_MAX_RETRIES)
+def update_conditions_for_quest(self, quest_id, start_from_user_id):
     quest = Quest.objects.filter(id=quest_id).first()
     if not quest:
         return
@@ -67,14 +66,11 @@ def update_conditions_for_quest(quest_id, start_from_user_id):
         user_id = user.id + 1
         user = User.objects.filter(id__gte=user_id).values('id').first()
         if user:
-            update_conditions_for_quest.apply_async(
-                kwargs={'quest_id': quest.id, 'start_from_user_id': user_id}, queue='default', countdown=20
-            )
+            self.apply_async(kwargs={'quest_id': quest.id, 'start_from_user_id': user_id}, queue='default', countdown=20)
 
 
-@shared_task(name='update_quest_conditions_all', max_retries=settings.CELERY_TASK_MAX_RETRIES)
-@try_except_task
-def update_quest_conditions_all(start_from_user_id):
+@shared_task(base=TransactionAwareTask, bind=True, name='update_quest_conditions_all', max_retries=settings.CELERY_TASK_MAX_RETRIES)
+def update_quest_conditions_all(self, start_from_user_id):
     if start_from_user_id == 1 and cache.get('update_conditions_all_task_waiting'):
         return
 
@@ -86,4 +82,4 @@ def update_quest_conditions_all(start_from_user_id):
     else:
         user = User.objects.filter(id__gte=uid + 1).values('id').first()
         if user:
-            update_quest_conditions_all.apply_async(args=[user['id']], queue='default', countdown=100)
+            self.apply_async(args=[user['id']], queue='default', countdown=100)
