d7b6249f5df7a6c66284a25fc6d309971d4c48af
Tylere Couture <tylerecouture@gmail.com>
2019-07-30 12:09:38 -0700

    email notifications via beat, remove celery worker -Q default

---
+++ src/profile_manager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class ProfileConfig(AppConfig):
+    name='profile_manager'
+    verbose_name = 'Profiles'
--- src/profile_manager/tasks.py
+++ src/profile_manager/tasks.py
@@ -20,15 +20,19 @@ email_notifications_schedule, _ = CrontabSchedule.objects.get_or_create(
 )
 
 def get_notification_emails():
-    users_to_email = User.objects.filter(profile__send_email_notifications=True)
+    users_to_email = User.objects.filter(profile__get_notifications_by_email=True)
+    print("Users to email: {}".format(str(users_to_email)))
     if len(users_to_email) > 95:
         print("Gmail is limited to sending 100 emails per day... gonna trim the list!")
     subject = 'Hackerspace notifications'
-    from_email_address = 'from@example.com'
+    from_email_address = 'timberline.hackerspace@gmail.com'
     notification_emails = []
     for user in users_to_email:
+        print("Looking for notifications for user: {}".format(user) )
         to_email_address = user.email
+        print("email addy: {}".format(user.email))
         unread_notifications = Notification.objects.all_unread(user)
+        print("Notifications: {}".format( str(unread_notifications)))
         # text_content = 'This is an important message.'
         text_content = str(unread_notifications)
         notification_emails.append( EmailMultiAlternatives(subject, text_content, from_email_address, [to_email_address]) )
