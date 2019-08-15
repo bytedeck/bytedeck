4012d39a85a3802c2e48b34eb35e8f2e0fd7660c
Tylere Couture <tylerecouture@gmail.com>
2019-08-15 13:26:01 -0700

    MarkRange migration

--- src/courses/models.py
+++ src/courses/models.py
@@ -23,16 +23,20 @@ class MarkRange(models.Model):
     name = models.CharField(max_length=50)
     minimum_mark = models.IntegerField(default=0, help_text="Minimum mark as a percentage from 0 to 100 (or higher)")
     active = models.BooleanField(default=True)
-    color = models.CharField(default='blue', help_text="An HTML color name or hax color value to represent this range")
+    color = models.CharField(
+        max_length=20, 
+        default='blue', 
+        help_text="An HTML color name or hax color value to represent this range"
+    )
     days = models.CharField(
-        validators=[validate_comma_separated_integer_list], max_length=13,
+        validators=[validate_comma_separated_integer_list], 
+        max_length=13,
         help_text='Comma seperated list of weekdays that this range is active, with Sun=1 eg: "2,4,6" for M, W, F.',
         default="1,2,3,4,5,6,7"
     )
     courses = models.ManyToManyField(
         "courses.course",
         blank=True,
-        null=True,
         help_text="Which courses this field is relevant to; blank means all courses."
     )
 
