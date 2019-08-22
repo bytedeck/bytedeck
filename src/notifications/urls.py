b9086524093fe65a4a27a687b8be5a9bfa99a212
Tylere Couture <tylerecouture@gmail.com>
2019-08-21 22:48:53 -0700

    remove notifcations from dropdown, needs styling

--- src/notifications/urls.py
+++ src/notifications/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^ajax/$', views.ajax, name='ajax'),  # function based view
     url(r'^read/(?P<id>\d+)/$', views.read, name='read'),  # function based view
     url(r'^read/all/$', views.read_all, name='read_all'),  # function based view
+    url(r'^ajax/mark/read/$', views.ajax_mark_read, name='ajax_mark_read'),
     # url(r'^options/$', views.options, name='options'), # function based view
 ]
