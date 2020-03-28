8733f811f882ce7ac04e3d473dc480f78f2c4693
Amit Garu <amitgaru2@gmail.com>
2020-03-28 14:19:55 +0545

    celery fixes for multi tenant

--- src/hackerspace_online/celery.py
+++ src/hackerspace_online/celery.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+
+from django.conf import settings
+from tenant_schemas_celery.app import CeleryApp
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackerspace_online.settings')
 
-app = Celery('hackerspace_online')
+app = CeleryApp('hackerspace_online')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -14,9 +16,9 @@ app = Celery('hackerspace_online')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+# @app.task(bind=True)
+# def debug_task(self):
+#     print('Request: {0!r}'.format(self.request))
