c45b56829483f823ffc6121e27084d83c73d99f8
Eugene Oliveros <eevoliveros@gmail.com>
2020-04-10 16:50:14 +0800

    Add success message in SiteConfig

--- src/siteconfig/views.py
+++ src/siteconfig/views.py
@@ -1,10 +1,13 @@
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.decorators import method_decorator
 from django.views.generic.edit import UpdateView
+
 from .models import SiteConfig
 
 
 @method_decorator(staff_member_required, name='dispatch')
-class SiteConfigUpdate(UpdateView):
+class SiteConfigUpdate(SuccessMessageMixin, UpdateView):
     model = SiteConfig
     fields = '__all__'
+    success_message = 'Settings updated successfully!'
