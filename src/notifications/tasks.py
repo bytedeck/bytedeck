eefb778a92a1a0b714fb5f5a86487da7c7d60b8e
Tylere Couture <tylerecouture@gmail.com>
2020-04-19 13:30:15 -0700

    temporarily disable notification emails by celery-beat

--- src/notifications/tasks.py
+++ src/notifications/tasks.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-from django_celery_beat.models import CrontabSchedule, PeriodicTask
-from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.core import mail
@@ -10,21 +8,15 @@ from django.template.loader import get_template
 
 from tenant_schemas.utils import get_public_schema_name
 
-# from hackerspace_online.celery import app
-from celery import shared_task
-from tenant_schemas.utils import get_tenant_model, tenant_context
+from hackerspace_online.celery import app
+# from celery import shared_task
+
 
 from siteconfig.models import SiteConfig
 
 from .models import Notification
 
 User = get_user_model()
-
-email_notifications_schedule, _ = CrontabSchedule.objects.get_or_create(
-    minute='0',
-    hour='5',
-    timezone=timezone.get_current_timezone()
-)
 
 
 def get_notification_emails(scheme_and_domain):
@@ -59,31 +51,24 @@ def get_notification_emails(scheme_and_domain):
 
 
 # Not an @app.task with tenant-schemas-celery because this is not a tenant aware task, i.e. we're doing it manually.
-@shared_task(name='notifications.tasks.send_email_notification_tenant')
-def send_email_notification_tenant(scheme_and_domain):
+# @app.task(name='notifications.tasks.send_email_notification_tenant')
+# def send_email_notification_tenant(scheme_and_domain):
+#     notification_emails = get_notification_emails(scheme_and_domain)
+#     connection = mail.get_connection()
+#     connection.send_messages(notification_emails)
+
+
+@app.task(name='notifications.tasks.email_notifications_to_users')
+def email_notifications_to_users(tenant_domain_url):
+
+    # can't use the obvious request.build_absolute_uri() cus there is no request!  So hack it together...
+    # this won't be accurate in development because it doesn't include the port `:8000`
+    scheme_and_domain = "https://{}".format(tenant_domain_url)
+    # add port for development:
+    if get_public_schema_name() == 'localhost':
+        scheme_and_domain += ":8000"
+
     notification_emails = get_notification_emails(scheme_and_domain)
     connection = mail.get_connection()
     connection.send_messages(notification_emails)
-
-
-# Not an @app.task with tenant-schemas-celery because this is not a tenant aware task, i.e. we're doing it manually.
-@shared_task(name='notifications.tasks.email_notifications_to_users')
-def email_notifications_to_users():
-    for tenant in get_tenant_model().objects.exclude(schema_name='public'):
-        with tenant_context(tenant):
-            # can't use the obvious request.build_absolute_uri() cus there is no request!  So hack it together...
-            # this won't be accurate in development because it doesn't include the port `:8000`
-            scheme_and_domain = "https://{}".format(tenant.domain_url)
-            # add port for development:
-            if get_public_schema_name() == 'localhost':
-                scheme_and_domain += ":8000"
-            send_email_notification_tenant.delay(scheme_and_domain)
-
-
-# CELERY-BEAT BROKEN - How to initialize this for each tenant?
-PeriodicTask.objects.get_or_create(
-    crontab=email_notifications_schedule,
-    name='Send daily email notifications',
-    task='notifications.tasks.email_notifications_to_users',
-    queue='default'
-)
+    # send_email_notification_tenant.delay(scheme_and_domain)
