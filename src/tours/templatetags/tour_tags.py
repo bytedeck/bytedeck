296a80f14ce27c0fd40f44b215ed92a55a52a2d2
Tylere Couture <tylerecouture@gmail.com>
2020-04-23 14:56:44 -0700

    remove tours app

--- src/tours/templatetags/tour_tags.py
+++
@@ -1,7 +0,0 @@
-# register = template.Library()
-
-# class ToursNode(template.Node):
-#     def __init__(self, format_string):
-#         self.format_string = format_string
-#     def render(self, context):
-#         return timezone.now().strftime(self.format_string)
