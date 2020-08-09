3b62f5ec3b129f2bda2dd6da3933d35962f045a9
Tylere Couture <tylerecouture@gmail.com>
2020-08-08 23:14:50 -0700

    New deck is created and redirected to the new tenant upon completion. #588 WIP

--- src/tenant/views.py
+++ src/tenant/views.py
@@ -1,6 +1,7 @@
 import functools
 
 from django.db import connection
+from django.contrib.sites.models import Site
 from django.http import Http404
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
@@ -56,3 +57,15 @@ class TenantCreate(PublicOnlyViewMixin, CreateView):
     model = Tenant
     form_class = TenantForm
     template_name = 'tenant/tenant_form.html'
+
+    def form_valid(self, form):
+        """ Copy the tenant name to the schema_name and the domain_url fields."""
+        tenant = form.save(commit=False)
+        # TODO: this is duplication of code in admin.py.  Move this into the Tenant model?  Perhaps as a pre-save hook?
+        tenant.schema_name = tenant.name.replace('-', '_')
+        tenant.domain_url = f"{tenant.name}.{Site.objects.get(id=1).domain}"
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """ Redirect to the newly created tenant."""
+        return self.object.get_root_url()
