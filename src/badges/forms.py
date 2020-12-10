33aba4138bf51c074898635235715a96d2bd13e5
Tylere Couture <tylerecouture@gmail.com>
2020-12-09 21:37:38 -0800

    Badge bulk granting with Select2 widgets

--- src/badges/forms.py
+++ src/badges/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django_select2.forms import Select2Widget, ModelSelect2MultipleWidget
+from django.forms.fields import MultipleChoiceField
+from django_select2.forms import Select2Widget, Select2MultipleWidget, ModelSelect2Widget, ModelSelect2MultipleWidget
 
 from profile_manager.models import Profile
 from .models import Badge, BadgeAssertion
@@ -30,29 +31,38 @@ class BadgeAssertionForm(forms.ModelForm):
         self.fields['user'].queryset = User.objects.order_by('profile')
         # It appears that sometimes a profile does not exist causing this to fail and the user field to not appear
         self.fields['user'].label_from_instance = lambda obj: "%s -- %s" % (
-            obj.profile if hasattr(obj, 'profile') else "", obj.username)
+            obj.profile if hasattr(obj, 'profile') else "", obj.username
+        )
 
 
 class BulkBadgeAssertionForm(forms.Form):
     # Queryset needs to be set on creation in __init__(), otherwise bad stuff happens upon initial migration
-    badge = forms.ModelChoiceField(queryset=None, required=True)
-    students = forms.ModelMultipleChoiceField(queryset=None)
-
-    def __init__(self, *args, **kwds):
-        super(BulkBadgeAssertionForm, self).__init__(*args, **kwds)
-        # Is this still relevant because no longer using djconfig?  Not broken so...
-        # Need to set queryset on init because config isn't available until apps are loaded.
-        # https://github.com/nitely/django-djconfig/issues/14
-        self.fields['students'].queryset = Profile.objects.all_for_active_semester()
-        self.fields['badge'].queryset = Badge.objects.all_manually_granted()
-
-        self.fields['students'].widget = ModelSelect2MultipleWidget(
+    badge = forms.ModelChoiceField(
+        queryset=Badge.objects.all(),
+        required=True, 
+        widget=ModelSelect2Widget(
+            model=Badge,
+            search_fields=['name__icontains'],
+        )
+    )
+    # students = forms.ModelMultipleChoiceField(queryset=None)
+    students = forms.ModelMultipleChoiceField(
+        # TODO just use the user objects here instead of profile
+        # Goign back to the user just to sort by profile string is...a hack.  How to do that properly?!
+        queryset=Profile.objects.all().order_by('user__profile'),
+        required=True,
+        widget=ModelSelect2MultipleWidget(
             model=Profile,
-            queryset=Profile.objects.all_for_active_semester(),
             search_fields=[
                 'first_name__istartswith',
                 'last_name__istartswith',
                 'preferred_name__istartswith',
                 'user__username__istartswith',
-            ]
-        )
+            ],
+            label_from_instance=lambda obj: "%s -- %s" % (obj, obj.user.username)
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['students'].label_from_instance = lambda obj: "%s -- %s" % (obj, obj.user.username)
