84cce85d289375e33c13365d65c03290e7acd5a9
Tylere Couture <tylerecouture@gmail.com>
2021-01-11 17:37:46 -0800

    remove reference to Hackerspace in click spam message; closes #787

--- src/static/js/jquery-double-submission.js
+++ src/static/js/jquery-double-submission.js
@@ -1,31 +1,13 @@
 // http://stackoverflow.com/questions/2830542/prevent-double-submission-of-forms-in-jquery/2830812#2830812
 
 // jQuery plugin to prevent double submission of forms
-// jQuery.fn.preventDoubleSubmission = function() {
-//   $(this).on('submit',function(e){
-//     var $form = $(this);
-//
-//     if ($form.data('submitted') === true) {
-//       // Previously submitted - don't submit again
-//       e.preventDefault();
-//     } else {
-//       // Mark it so that the next submit can be ignored
-//       $form.data('submitted', true);
-//     }
-//   });
-//
-//   // Keep chainability
-//   return this;
-// };
-
-// jQuery plugin to prevent double submission of forms
 jQuery.fn.preventDoubleSubmission = function () {
     $(this).on('submit', function (e) {
         var $form = $(this);
 
         if ($form.data('submitted') === true) {
             // Previously submitted - don't submit again
-            alert('Ok Clickyhappy McClickface, how about you have a little patience and give me some time to process your request?  Thanks! - Hackerspace AI.');
+            alert('Ok Clickyhappy McClickface, how about you have a little patience and give me some time to process your request?  Thanks!');
             e.preventDefault();
         } else {
             // Mark it so that the next submit can be ignored
