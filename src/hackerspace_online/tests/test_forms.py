8cba9eedd01d1f0d062533c792e03aaa2ab8736e
Eugene Oliveros <eevoliveros@gmail.com>
2022-03-10 13:18:48 +0800

    Add test to login and signup forms

--- src/hackerspace_online/tests/test_forms.py
+++ src/hackerspace_online/tests/test_forms.py
@@ -5,7 +5,7 @@ from django.shortcuts import reverse
 from django_tenants.test.cases import TenantTestCase
 from django_tenants.test.client import TenantClient
 
-from hackerspace_online.forms import CustomSignupForm, PublicContactForm
+from hackerspace_online.forms import CustomSignupForm, PublicContactForm, CustomLoginForm
 
 User = get_user_model()
 
@@ -62,6 +62,56 @@ class CustomSignUpFormTest(TenantTestCase):
         self.assertRedirects(response, reverse('quests:quests'))
         user = User.objects.get(username="username")
         self.assertEqual(user.first_name, "firsttest")
+
+    def test_sign_up_via_post_upcase_username(self):
+        self.client = TenantClient(self.tenant)
+        form_data = {
+            'username': "TestUser",
+            'first_name': "firsttest",
+            'last_name': "Lasttest",
+            'access_code': "314159",
+            'password1': "password",
+            'password2': "password"
+        }
+        response = self.client.post(reverse('account_signup'), form_data, follow=True,)
+        self.assertRedirects(response, reverse('quests:quests'))
+
+        with self.assertRaises(User.DoesNotExist):
+            User.objects.get(username=form_data['username'])
+
+        user = User.objects.get(username=form_data['username'].lower())
+
+        self.assertIsNotNone(user)
+
+
+class CustomLoginFormTest(TenantTestCase):
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='testuser')
+
+    def test_init(self):
+        CustomLoginForm()
+
+    def test_valid_data(self):
+        form = CustomLoginForm(
+            {
+                'login': 'testuser',
+                'password': 'testuser',
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_login_with_upcase_username(self):
+        """
+        User should still be able to login regardless of the username case
+        """
+        self.client = TenantClient(self.tenant)
+        form_data = {
+            'login': 'TestUser',
+            'password': 'testuser'
+        }
+        response = self.client.post(reverse('account_login'), form_data, follow=True)
+        self.assertRedirects(response, reverse('quests:quests'))
 
 
 class PublicContactFormTest(TenantTestCase):
