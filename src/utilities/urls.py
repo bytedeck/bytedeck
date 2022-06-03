80815090f01d4192a9e57b6a3af8905371e71ca4
9921622 <marc.alfre.go@gmail.com>
2022-06-02 18:39:34 -0700

    feat: added CRUD for flatpages; Closes #776

--- src/utilities/urls.py
+++ src/utilities/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 
 from utilities import views
 
@@ -8,4 +9,9 @@ urlpatterns = [
     # url(r'^$', views.suggestion_list, name='list'),
     url(r'^videos/$', views.videos, name='videos'),
 
+    # flatpages
+    path('custom-pages/', views.FlatPageListView.as_view(), name='flatpage-list'),
+    path('custom-pages/create/', views.FlatPageCreateView.as_view(), name='flatpage-create'),
+    path('custom-pages/edit/<int:pk>/', views.FlatPageUpdateView.as_view(), name='flatpage-edit'),
+    path('custom-pages/delete/<int:pk>/', views.FlatPageDeleteView.as_view(), name='flatpage-delete'),
 ]
--- src/utilities/views.py
+++ src/utilities/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
+
+
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.forms import FlatpageForm
 from .models import VideoResource
 from utilities.forms import VideoForm
-from tenant.views import non_public_only_view
+from tenant.views import non_public_only_view, NonPublicOnlyViewMixin
 
 
 @non_public_only_view
@@ -13,3 +22,44 @@ def videos(request):
         form.save()
     context = {'videos': videos, 'heading': "Video Resources", 'form': form}
     return render(request, 'utilities/videos.html', context)
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class FlatPageCreateView(NonPublicOnlyViewMixin, CreateView):
+    model = FlatPage
+    form_class = FlatpageForm
+    template_name = "flatpages/flatpage-form.html"
+
+    def get_success_url(self):
+        return reverse('utilities:flatpage-list')
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class FlatPageUpdateView(NonPublicOnlyViewMixin, UpdateView):
+    model = FlatPage
+    form_class = FlatpageForm
+    template_name = "flatpages/flatpage-form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['update'] = True
+        context['flatpage'] = self.get_object()
+        return context
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class FlatPageDeleteView(NonPublicOnlyViewMixin, DeleteView):
+    model = FlatPage
+    template_name = "flatpages/flatpage-delete.html"
+
+    def get_success_url(self):
+        return reverse('utilities:flatpage-list')
+
+
+class FlatPageListView(NonPublicOnlyViewMixin, ListView):
+    model = FlatPage
+    template_name = "flatpages/flatpage-list.html"
+    context_object_name = 'flatpages'
+
+    def get_queryset(self):
+        return FlatPage.objects.all()
