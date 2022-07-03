350d629482fc4cb5e0d314221f23d1e60010ee78
Tylere Couture <tylerecouture@gmail.com>
2022-07-02 23:15:50 -0700

    remove unused import

--- src/hackerspace_online/urls.py
+++ src/hackerspace_online/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
 
 from badges.views import AchievementRedirectView
 from hackerspace_online import views
