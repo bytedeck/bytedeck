d5335f0a1b56f9bb6200a165936b47ada9de9f48
Dylan Patterson <dylanrgpatterson@gmail.com>
2022-07-25 19:27:28 -0700

    default message quest notifies deck owner; closes #1104

--- src/tenant/tests/test_initialization.py
+++ src/tenant/tests/test_initialization.py
@@ -2,8 +2,9 @@ from django_tenants.test.cases import TenantTestCase
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-from badges.models import BadgeRarity
-from courses.models import MarkRange
+from badges.models import Badge, BadgeRarity, BadgeType
+from courses.models import Block, Course, Grade, MarkRange, Rank
+from quest_manager.models import Category, Quest
 from utilities.models import MenuItem
 
 
@@ -12,32 +13,8 @@ User = get_user_model()
 
 class TenantInitializationTest(TenantTestCase):
 
-    def test_default_badge_rarities_created(self):
-        """When a new tenant is created, the initialization script should create 6 default BadgeRarity objects."""
-        self.assertTrue(BadgeRarity.objects.filter(name="Common").exists())
-        self.assertTrue(BadgeRarity.objects.filter(name="Uncommon").exists())
-        self.assertTrue(BadgeRarity.objects.filter(name="Rare").exists())
-        self.assertTrue(BadgeRarity.objects.filter(name="Epic").exists())
-        self.assertTrue(BadgeRarity.objects.filter(name="Legendary").exists())
-        self.assertTrue(BadgeRarity.objects.filter(name="Mythic").exists())
-    
-    def test_only_default_rarities_created(self):
-        self.assertEqual(BadgeRarity.objects.count(), 6)
-
-    def test_default_mark_ranges_created(self):
-        """ 
-            When a new tenant is created, the initialization script should create 3 default Markrange objects.
-        """ 
-        for name in ["A", "B", "Pass"]:
-            self.assertTrue(MarkRange.objects.filter(name=name).exists())
-
-    def test_default_menu_items_created(self):
-        self.assertTrue(MenuItem.objects.filter(label="Ranks List").exists())
-
     def test_admin_created(self):
-        """ 
-            Check if admin superuser is created upon initialization
-        """ 
+        """ Check if admin superuser is created upon initialization """ 
         username = "admin"
         password = settings.TENANT_DEFAULT_ADMIN_PASSWORD
 
@@ -49,11 +26,9 @@ class TenantInitializationTest(TenantTestCase):
 
         success = self.client.login(username=username, password=password)
         self.assertTrue(success)
-
+    
     def test_owner_created(self):
-        """ 
-            Check if deck_owner is created upon initialization
-        """ 
+        """ Check if deck_owner is created upon initialization """ 
         username = "owner"
         password = settings.TENANT_DEFAULT_OWNER_PASSWORD
 
@@ -65,3 +40,98 @@ class TenantInitializationTest(TenantTestCase):
 
         success = self.client.login(username=username, password=password)
         self.assertTrue(success)
+
+    def test_default_course_created(self):
+        """ Initialization script should create a default Course object. """
+        self.assertTrue(Course.objects.filter(title="Default").exists())
+    
+    def test_default_block_created(self):
+        """ Initialization scripts should create a default Block object. """
+        self.assertTrue(Block.objects.filter(block="Default").exists())
+    
+    def test_default_mark_ranges_created(self):
+        """ Initialization script should create 3 default MarkRange objects. """ 
+        for name in ["A", "B", "Pass"]:
+            self.assertTrue(MarkRange.objects.filter(name=name).exists())
+
+    def test_default_ranks_created(self):
+        """ 
+            Initialization script should create 13 default Rank objects.
+            Test shortened for brevity.
+        """
+        self.assertTrue(Rank.objects.filter(name="Digital Noob").exists())
+        self.assertEqual(Rank.objects.count(), 13)
+
+    def test_default_grades_created(self):
+        """ Initialization script should create 5 default Grade objects. """
+        for name in ["8", "9", "10", "11", "12"]:
+            self.assertTrue(Grade.objects.filter(name=name).exists())
+
+    def test_default_menu_items_created(self):
+        """ Initialization script should create a default MenuItem object. """
+        self.assertTrue(MenuItem.objects.filter(label="Ranks List").exists())
+
+    def test_default_badge_types_created(self):
+        """ Initialization script should create 3 default BadgeType objects. """
+        self.assertTrue(BadgeType.objects.filter(name="Talent").exists())
+        self.assertTrue(BadgeType.objects.filter(name="Award").exists())
+        self.assertTrue(BadgeType.objects.filter(name="Team").exists())
+
+    def test_default_badge_rarities_created(self):
+        """ Initialization script should create 6 default BadgeRarity objects. """
+        self.assertTrue(BadgeRarity.objects.filter(name="Common").exists())
+        self.assertTrue(BadgeRarity.objects.filter(name="Uncommon").exists())
+        self.assertTrue(BadgeRarity.objects.filter(name="Rare").exists())
+        self.assertTrue(BadgeRarity.objects.filter(name="Epic").exists())
+        self.assertTrue(BadgeRarity.objects.filter(name="Legendary").exists())
+        self.assertTrue(BadgeRarity.objects.filter(name="Mythic").exists())
+
+    def test_only_default_rarities_created(self):
+        """ 
+            Badge rarities shouldn't have overlapping values, and the default rarities cover every percentage value.
+            Any additional BadgeRarity objects existing at initialization could cause conflicts.
+        """
+        self.assertEqual(BadgeRarity.objects.count(), 6)
+
+    def test_default_badges_created(self):
+        """ Initialization script should create 7 default Badge objects. """
+        self.assertTrue(Badge.objects.filter(name="Penny").exists())
+        self.assertTrue(Badge.objects.filter(name="Nickel").exists())
+        self.assertTrue(Badge.objects.filter(name="Dime").exists())
+        self.assertTrue(Badge.objects.filter(name="ByteDeck Proficiency").exists())
+        self.assertTrue(Badge.objects.filter(name="Red Team").exists())
+        self.assertTrue(Badge.objects.filter(name="Green Team").exists())
+        self.assertTrue(Badge.objects.filter(name="Blue Team").exists())
+
+    def test_default_badge_icons(self):
+        """
+            Empty because Django tests involving static files are prone to breakage.
+            Come back to this once testing static files is made clearer.
+        """
+        pass
+    
+    def test_default_campaign_created(self):
+        """ Initialization script should create a default Category (Campaign) object. """
+        self.assertTrue(Category.objects.filter(title="Orientation").exists())
+
+    def test_default_quests_created(self):
+        """ Initialization script should create 6 default Quest objects. """
+        self.assertTrue(Quest.objects.filter(name="Welcome to ByteDeck!").exists())
+        self.assertTrue(Quest.objects.filter(name="ByteDeck Class Contract").exists())
+        self.assertTrue(Quest.objects.filter(name="Create an Avatar").exists())
+        self.assertTrue(Quest.objects.filter(name="Screenshots").exists())
+        self.assertTrue(Quest.objects.filter(name="Who owns your creations?").exists())
+        self.assertTrue(Quest.objects.filter(name="Send your teacher a Message").exists())
+
+    def test_default_quest_icons(self):
+        """
+            Empty because Django tests involving static files are prone to breakage.
+            Come back to this once testing static files is made clearer.
+        """
+        pass
+    
+    def test_default_message_quest_notifies_owner(self):
+        """ The quest "Send your teacher a Message" should have the deck owner assigned as the specific teacher to notify by default. """
+        message_quest = Quest.objects.filter(name="Send your teacher a Message").first()
+        owner = User.objects.filter(username="owner", is_staff=True).first()
+        self.assertEqual(message_quest.specific_teacher_to_notify, owner)
