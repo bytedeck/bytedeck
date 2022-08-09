5a6f91d72a44b01d3984fa3ff16deb79ec5a6a4b
9921622 <39788517+9921622@users.noreply.github.com>
2022-08-08 21:24:17 -0700

    Feat: User specific Tag detail view; Closes #1125 (#1134)

--- src/tags/urls.py
+++ src/tags/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
 
     path('', views.TagList.as_view(), name='list'),
     path('create/', views.TagCreate.as_view(), name='create'),
-    path('<int:pk>/', views.TagDetail.as_view(), name='detail'),
+    path('<int:pk>/', views.TagDetailStaff.as_view(), name='detail_staff'),
+    path('<int:tag_pk>/<int:user_pk>/', views.TagDetailStudent.as_view(), name='detail_student'),
     path('update/<int:pk>/', views.TagUpdate.as_view(), name='update'),
     path('delete/<int:pk>/', views.TagDelete.as_view(), name='delete'),
 ]
--- src/tags/views.py
+++ src/tags/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views.generic import DetailView
@@ -8,8 +9,8 @@ from django.views.generic.list import ListView
 
 from hackerspace_online.decorators import staff_member_required
 
-from badges.models import Badge
-from quest_manager.models import Quest
+from badges.models import Badge, BadgeAssertion
+from quest_manager.models import Quest, QuestSubmission
 
 from tags.forms import TagForm
 
@@ -37,20 +38,67 @@ from taggit.models import Tag
 
 #         return qs
 
+User = get_user_model()
+
 
 class TagList(NonPublicOnlyViewMixin, LoginRequiredMixin, ListView):
     model = Tag
     template_name = 'tags/list.html'
 
 
-class TagDetail(NonPublicOnlyViewMixin, LoginRequiredMixin, DetailView):
+class TagDetailMixin(NonPublicOnlyViewMixin, LoginRequiredMixin, DetailView):
     model = Tag
     template_name = 'tags/detail.html'
 
     def get_context_data(self, **kwargs):
-        kwargs['quests'] = Quest.objects.filter(tags__name=self.object.name)
-        kwargs['badges'] = Badge.objects.filter(tags__name=self.object.name)
+        kwargs['quests'] = self.get_quest_queryset()
+        kwargs['badges'] = self.get_badge_queryset()
+
         return super().get_context_data(**kwargs)
+
+
+class TagDetailStudent(TagDetailMixin, NonPublicOnlyViewMixin, LoginRequiredMixin, DetailView):
+
+    def get_object(self):
+        pk = self.request.GET.get('tag_pk')
+        return Tag.objects.get(pk=pk)
+
+    def get_user_object(self):
+        pk = self.request.GET.get('user_pk')
+        return User.objects.get(pk=pk)
+
+    def get(self, *args, **kwargs):
+        self.request.GET = kwargs
+        self.object = self.get_object()
+        self.user = self.get_user_object()
+
+        return super().get(*args, **kwargs)
+
+    def get_quest_queryset(self):
+        # returns all quest objects related to tag and user
+        return QuestSubmission.objects.filter(
+                    user=self.user, quest__tags__name=self.object.name
+                ).order_by('quest__id').distinct('quest__id').select_related('quest').all()
+
+    def get_badge_queryset(self):
+        # returns all badge objects related to tag and user
+        return BadgeAssertion.objects.filter(
+                    user=self.user, badge__tags__name=self.object.name
+                ).order_by('badge__id').distinct('badge__id').select_related('badge').all()
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_obj'] = self.user
+        return super().get_context_data(**kwargs) 
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class TagDetailStaff(TagDetailMixin, NonPublicOnlyViewMixin, LoginRequiredMixin, DetailView):
+
+    def get_quest_queryset(self):
+        return Quest.objects.filter(tags__name=self.object.name)
+
+    def get_badge_queryset(self):
+        return Badge.objects.filter(tags__name=self.object.name)
 
 
 @method_decorator(staff_member_required, name='dispatch')
@@ -88,7 +136,6 @@ class TagDelete(NonPublicOnlyViewMixin, DeleteView):
     success_url = reverse_lazy('tags:list')
 
     def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs).copy()
-        ctx['quests'] = Quest.objects.filter(tags__id=self.object.id)
-        ctx['badges'] = Badge.objects.filter(tags__id=self.object.id)
-        return ctx
+        kwargs['quests'] = Quest.objects.filter(tags__id=self.object.id)
+        kwargs['badges'] = Badge.objects.filter(tags__id=self.object.id)
+        return super().get_context_data(**kwargs)
