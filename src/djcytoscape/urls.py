bf62360e661fe1a216ba55702ae7ae8b281f52da
9921622 <marc.alfre.go@gmail.com>
2022-08-15 17:50:00 -0700

    Feat: Map create form uses DAL widget; Closes #1141

--- src/djcytoscape/urls.py
+++ src/djcytoscape/urls.py
@@ -6,8 +6,8 @@ app_name = 'djcytoscape'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
-    url(r'^generate/(?P<quest_id>[0-9]+)/(?P<scape_id>[0-9]+)$', views.generate_map, name='generate_map'),
-    url(r'^generate/$', views.generate_map, name='generate_unseeded'),
+    url(r'^generate/(?P<quest_id>[0-9]+)/(?P<scape_id>[0-9]+)$', views.ScapeGenerateMap.as_view(), name='generate_map'),
+    url(r'^generate/$', views.ScapeGenerateMap.as_view(), name='generate_unseeded'),
     url(r'^primary/$', views.primary, name='primary'),
 
     url(r'^(?P<scape_id>[0-9]+)/$', views.quest_map, name='quest_map'),
