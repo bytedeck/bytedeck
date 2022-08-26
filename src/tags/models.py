1007a443fd07226b08f84207bd8f0a4b1e0a0348
9921622 <39788517+9921622@users.noreply.github.com>
2022-08-25 22:09:58 -0700

    Fix: Tags by XP needs to account for quest.max_xp and questsubmission.xp_requested; Closes #1182 (#1184)

--- src/tags/models.py
+++ src/tags/models.py
@@ -1,6 +1,7 @@
 from django.apps import apps
 from django.db import models
 from django.db.models import Sum
+from django.db.models.functions import Greatest
 
 from taggit.models import Tag
 from taggit.managers import TaggableManager
@@ -76,6 +77,65 @@ def get_badge_assertion_by_tags(user, tags):
         semester=SiteConfig.get().active_semester, 
         do_not_grant_xp=False
     ).distinct()
+    
+
+def get_quest_submission_total_xp(user, tags):
+    """ 
+    returns total quest xp related to user and tags
+
+    Args:
+        user (UserModel): user object
+        tags (list[str], Queryset<Tag>): a queryset or list of Tag objects
+
+    Returns:
+        int: total quest xp related to user and tags
+    """
+    total_xp = 0
+    submissions = get_quest_submission_by_tag(user, tags).distinct()
+
+    # annotate with xp_earned, since xp could come from xp_requested on the submission, or from the quest's xp value
+    # take the greater value (since custom entry have the quest.xp as a minimum)
+    submissions = submissions.annotate(xp_earned=Greatest('quest__xp', 'xp_requested'))
+
+    # calculate sum here since using annotate(sum()) will somehow count the amount of tags
+    # this just calculates the xp sum of submission related to quest
+    # key: quest.id  value: quest total xp
+    quest_xp_sum = {}
+    submissions_xp = submissions.values('id', 'quest', 'xp_earned')
+    for sub_xp in submissions_xp:
+        quest_xp_sum.setdefault(sub_xp['quest'], 0)
+        quest_xp_sum[sub_xp['quest']] += sub_xp['xp_earned']
+
+    # putting quest as value will squish all same quests to one QS
+    submissions = submissions.values('quest', 'quest__max_xp')
+
+    for sub in submissions:
+        xp = quest_xp_sum[sub['quest']]
+
+        if sub['quest__max_xp'] == -1:  # no limit
+            total_xp += xp
+        else:
+            # Prevent xp going over the maximum gainable xp
+            # quest__max_xp is None if quest is deleted, so `or 0`
+            total_xp += min(xp, sub['quest__max_xp'] or 0)  
+
+    return total_xp
+
+
+def get_badge_assertion_total_xp(user, tags):
+    """ 
+    returns total quest xp related to user and tags
+
+    Args:
+        user (UserModel): user object
+        tags (list[str], Queryset<Tag>): a queryset or list of Tag objects
+
+    Returns:
+        int: total badge xp related to user and tags
+    """
+    # sum can be none since aggregate() will always return a dictionary of keys of whatever arg you put in.
+    # if there are no badges or quests linked to the user, aggregate will return a dictionary like: { 'badge__xp__sum': None }
+    return get_badge_assertion_by_tags(user, tags).aggregate(Sum("badge__xp"))['badge__xp__sum'] or 0
 
 
 def total_xp_by_tags(user, tags):
@@ -89,12 +149,7 @@ def total_xp_by_tags(user, tags):
     Returns:
         int : total xp of user related to tag
     """
-    # sum can be none since aggregate() will always return a dictionary of keys of whatever arg you put in.
-    # if there are no badges or quests linked to the user, aggregate will return a dictionary like: { 'badge__xp__sum': None }
-    quest_sum = get_quest_submission_by_tag(user, tags).aggregate(Sum("quest__xp"))['quest__xp__sum'] or 0
-    badge_sum = get_badge_assertion_by_tags(user, tags).aggregate(Sum("badge__xp"))['badge__xp__sum'] or 0
-
-    return quest_sum + badge_sum
+    return get_quest_submission_total_xp(user, tags) + get_badge_assertion_total_xp(user, tags)
 
 
 def get_user_tags_and_xp(user):
