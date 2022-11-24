ba3ca851d210c494d8919445c856e1040bd23fc0
Basil Shubin <basil.shubin@gmail.com>
2022-11-24 13:55:54 +0600

    Fix #1154, Fix #1153

--- src/prerequisites/urls.py
+++
@@ -1,8 +0,0 @@
-from prerequisites.forms import PrereqFormInline
-
-# app_name = 'prerequisites' # django-autocomplete-light doesn't work (by default anyway) with namespaces
-
-urlpatterns = []
-
-#  https://django-autocomplete-light.readthedocs.io/en/master/gfk.html#register-the-view-for-the-form
-urlpatterns.extend(PrereqFormInline.as_urls())
