117cb0f674257175f163a3ac3184d2a2ec1d886f
Tylere Couture <tylerecouture@gmail.com>
2023-06-17 15:29:03 -0700

    Add comment template tag tests; remove unused tag

--- src/comments/templatetags/comment_tags.py
+++ src/comments/templatetags/comment_tags.py
@@ -1,16 +1,15 @@
 import os
 
 from django import template
-from django.contrib.contenttypes.models import ContentType
 
 register = template.Library()
 
 
-@register.filter
-def content_type(obj):
-    if not obj:
-        return False
-    return ContentType.objects.get_for_model(obj)
+# @register.filter
+# def content_type(obj):
+#     if not obj:
+#         return False
+#     return ContentType.objects.get_for_model(obj)
 
 
 @register.filter
