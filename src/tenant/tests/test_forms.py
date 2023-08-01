3aee3d1f0fd519080566436213ac8f8a797fc3b0
Basil Shubin <basil.shubin@gmail.com>
2023-08-01 13:23:43 +0600

    tests: test for "max_length" property

--- src/tenant/tests/test_forms.py
+++ src/tenant/tests/test_forms.py
@@ -1,8 +1,15 @@
 # With help from https://stackoverflow.com/questions/6498488/testing-admin-modeladmin-in-django
+import random
+import string
+
+from django.contrib.auth import get_user_model
+
 from django_tenants.test.cases import TenantTestCase
 
 from tenant.forms import TenantForm
 from tenant.models import Tenant
+
+User = get_user_model()
 
 
 class TenantFormTest(TenantTestCase):
@@ -40,6 +47,47 @@ class TenantFormTest(TenantTestCase):
             "first_name": "John",
             "last_name": "Doe",
             "email": "john.doe@example.com",
+        }
+        form = TenantForm(data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_max_length(self):
+        """
+        Test if form fields has set correct `max_length` property.
+        """
+        def generate_random_string(length=128):
+            return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+        # first case, using `first_name` longer than `User.first_name` can accept,
+        # should fail with form error
+        data = {
+            "first_name": generate_random_string(User._meta.get_field("first_name").max_length + 1),
+        }
+        form = TenantForm(data)
+        self.assertEqual(form.errors["first_name"], ["Ensure this value has at most 150 characters (it has 151)."])
+
+        # second case, using `last_name` longer than `User.last_name` can accept,
+        # should fail with form error
+        data = {
+            "last_name": generate_random_string(User._meta.get_field("last_name").max_length + 1),
+        }
+        form = TenantForm(data)
+        self.assertEqual(form.errors["last_name"], ["Ensure this value has at most 150 characters (it has 151)."])
+
+        # third case, using `email` longer than `User.email` can accept,
+        # should fail with form error
+        data = {
+            "email": generate_random_string(User._meta.get_field("email").max_length) + "@example.com",
+        }
+        form = TenantForm(data)
+        self.assertEqual(form.errors["email"], ["Ensure this value has at most 254 characters (it has 266)."])
+
+        # final case, submit complete (full) form
+        data = {
+            "name": "default",
+            "first_name": generate_random_string(User._meta.get_field("first_name").max_length),
+            "last_name": generate_random_string(User._meta.get_field("last_name").max_length),
+            "email": generate_random_string(User._meta.get_field("email").max_length - 12) + "@example.com",
         }
         form = TenantForm(data)
         self.assertTrue(form.is_valid())
