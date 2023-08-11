345b484ecf4c1911f2923b7b08466ad2598f2415
Basil Shubin <basil.shubin@gmail.com>
2023-08-11 17:53:00 +0600

    fix: verbose error message

--- src/hackerspace_online/middleware.py
+++ src/hackerspace_online/middleware.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import connections
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.core.exceptions import RequestDataTooBig
+from django.template.defaultfilters import filesizeformat
 
 
 class ForceDebugCursorMiddleware:
@@ -19,6 +21,7 @@ class RequestDataTooBigMiddleware:
     """
     Custom middleware to handle requests that exceed the settings.DATA_UPLOAD_MAX_MEMORY_SIZE
     """
+
     def __init__(self, get_response):
         self.get_response = get_response
 
@@ -32,9 +35,16 @@ class RequestDataTooBigMiddleware:
         except RequestDataTooBig:
             # if the size of the request (excluding any file uploads) exceeded settings.DATA_UPLOAD_MAX_MEMORY_SIZE.
 
+            msg = (
+                "This requests exceeds the maximum size of {}. This is likely caused by"
+                " one of the text fields contains too much data, either from text (including whitespace)"
+                " or a large data URI (an image or other data encoded as text).".format(
+                    filesizeformat(settings.DATA_UPLOAD_MAX_MEMORY_SIZE)
+                )
+            )
+
             # generate a more sensible response and redirect back
-            messages.add_message(
-                request, messages.ERROR, "The size of the request is too large. Please reduce its size and try again.")
+            messages.add_message(request, messages.ERROR, msg)
             return HttpResponseRedirect(request.build_absolute_uri())
 
         # ...as if nothing has happened
