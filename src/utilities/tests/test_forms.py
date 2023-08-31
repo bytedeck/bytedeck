6eee8885deb1810a7f6b3adb5dab8c055ef83ae7
Eugene Oliveros <yujin@jinyuz.dev>
2023-08-31 08:14:58 +0800

    Add test for side menu form

--- src/utilities/tests/test_forms.py
+++ src/utilities/tests/test_forms.py
@@ -21,7 +21,7 @@ class MenuItemFormTest(TenantTestCase):
             'visible': True,
         }
         form = MenuItemForm(data=form_data)
-        self.assertTrue(form.is_valid)
+        self.assertTrue(form.is_valid())
 
     def test_MenuItem_form_allow_absolute_urls(self):
         form_data = {
@@ -33,4 +33,19 @@ class MenuItemFormTest(TenantTestCase):
             'visible': True,
         }
         form = MenuItemForm(data=form_data)
-        self.assertTrue(form.is_valid)
+        self.assertTrue(form.is_valid())
+
+    def test_MenuItem_form_is_side_menu(self):
+        form_data = {
+            'label': 'New Menu Item',
+            'fa_icon': 'link',
+            'url': 'https://github.com/bytedeck/bytedeck',
+            'open_link_in_new_tab': False,
+            'sort_order': 0,
+            'visible': True,
+        }
+        form = MenuItemForm(data=form_data, is_side_menu=True)
+        self.assertTrue(form.is_valid())
+
+        form.save()
+        self.assertTrue(form.instance.is_side_menu)
