8fef7a8818dbaaabed17543f58517ca7cb1a70bc
Basil Shubin <basil.shubin@gmail.com>
2023-09-07 18:15:24 +0600

    tests: creating with duplicate initial objects

--- src/djcytoscape/tests/test_fields.py
+++ src/djcytoscape/tests/test_fields.py
@@ -8,12 +8,26 @@ from djcytoscape.models import CytoScape
 class CytoscapeGFKChoiceFieldTest(TenantTestCase):
 
     def test_queryset(self):
+        """ Quick test to see if the hardcoded model list is equal to CytoScape.ALLOWED_INITIAL_CONTENT_TYPES """
         from djcytoscape.forms import CytoscapeGFKChoiceField
 
-        """ Quick test to see if the hardcoded model list is equal to CytoScape.ALLOWED_INITIAL_CONTENT_TYPES """
         dynamically_loaded_models = [
             ct.model_class() for ct in ContentType.objects.filter(CytoScape.ALLOWED_INITIAL_CONTENT_TYPES)]
 
         f = CytoscapeGFKChoiceField()
 
         self.assertEqual(dynamically_loaded_models, [qs.model for qs in f.queryset.get_querysets()])
+
+    def test_queryset_is_filterable(self):
+        from djcytoscape.forms import CytoscapeGFKChoiceField
+
+        content_type = ContentType.objects.filter(CytoScape.ALLOWED_INITIAL_CONTENT_TYPES).first()
+        obj = content_type.model_class().objects.first()
+
+        f = CytoscapeGFKChoiceField()
+        self.assertIn(obj, [o for qs in f.queryset.get_querysets() for o in qs])
+
+        CytoScape.generate_map(obj, "test")
+
+        f = CytoscapeGFKChoiceField()
+        self.assertNotIn(obj, [o for qs in f.queryset.get_querysets() for o in qs])
