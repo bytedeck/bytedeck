823d8559cb80f259857c9a8f3cdf57e14dfded04
TimberlineHackerspace <timberline.hackerspace@gmail.com>
2024-02-29 15:57:39 -0800

    Remove old unused MultiFileField and MultiFileInput

--- src/utilities/forms.py
+++ src/utilities/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.forms import FlatpageForm
 
@@ -55,85 +54,6 @@ class VideoForm(forms.ModelForm):
     class Meta:
         model = VideoResource
         fields = ["title", "video_file"]
-
-
-class MultiFileInput(forms.FileInput):
-    def render(self, name, value, attrs=None):
-        if attrs is None:
-            attrs = {}
-        attrs['multiple'] = 'multiple'
-        # attrs['class'] += 'btn'
-        return super().render(name, None, attrs=attrs)
-
-    def value_from_datadict(self, data, files, name):
-        if hasattr(files, 'getlist'):
-            return files.getlist(name)
-        else:
-            return [files.get(name)]
-
-
-class MultiFileField(forms.FileField):
-    widget = MultiFileInput
-    default_error_messages = {
-        'min_num': "Ensure at least %(min_num)s files are uploaded (received %(num_files)s).",
-        'max_num': "Ensure at most %(max_num)s files are uploaded (received %(num_files)s).",
-        'file_size': "File: %(uploaded_file_name)s, exceeded maximum upload size."
-    }
-
-    def __init__(self, *args, **kwargs):
-        self.min_num = kwargs.pop('min_num', 0)
-        self.max_num = kwargs.pop('max_num', None)
-        self.maximum_file_size = kwargs.pop('maximum_file_size', None)
-        super().__init__(*args, **kwargs)
-
-    def to_python(self, data):
-        ret = []
-        for item in data:
-            ret.append(super().to_python(item))
-        return ret
-
-    # def validate(self, data):
-    #     super(MultiFileField, self).validate(data)
-    #     num_files = len(data)
-    #     if len(data) and not data[0]:
-    #         num_files = 0
-    #     if num_files < self.min_num:
-    #         raise ValidationError(self.error_messages['min_num'] % {'min_num': self.min_num, 'num_files': num_files})
-    #         return
-    #     elif self.max_num and  num_files > self.max_num:
-    #         raise ValidationError(self.error_messages['max_num'] % {'max_num': self.max_num, 'num_files': num_files})
-    #     for uploaded_file in data:
-    #         if uploaded_file.size > self.maximum_file_size:
-    #             raise ValidationError(self.error_messages['file_size'] % { 'uploaded_file_name': uploaded_file.name})
-
-    def clean(self, data, initial=None):
-        super().clean(data, initial=None)
-        num_files = len(data)
-        if len(data) and not data[0]:
-            num_files = 0
-        if num_files < self.min_num:
-            raise ValidationError(self.error_messages['min_num'] % {'min_num': self.min_num, 'num_files': num_files})
-            return
-        elif self.max_num and num_files > self.max_num:
-            raise ValidationError(self.error_messages['max_num'] % {'max_num': self.max_num, 'num_files': num_files})
-        if num_files > 0:
-            for uploaded_file in data:
-                if uploaded_file.size > self.maximum_file_size:
-                    raise ValidationError(self.error_messages['file_size'] % {'uploaded_file_name': uploaded_file.name})
-        return data
-
-        # def clean(self, data, initial=None):
-        #     super(MultiFileField, self).clean(data, initial)
-        #     try:
-        #         # content_type = file.content_type
-        #         # if content_type in self.content_types:
-        #         if file._size > self.max_upload_size:
-        #             raise ValidationError(_('Please keep filesize under %s. Current filesize %s') % (
-        #                 filesizeformat(self.max_upload_size), filesizeformat(file._size)))
-        #         # else:
-        #             # raise ValidationError(_('Filetype not supported.'))
-        #     except AttributeError:
-        #         pass
 
 
 class CustomFlatpageForm(FlatpageForm):
