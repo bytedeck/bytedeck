44d6f8674bba7b299083f4c9897735a0fd4f86a9
thefoxwithin <marcus@walkswithin.com>
2025-07-13 16:33:07 -0700

    some minor changes updating the migrations
    fixed unboundlocalerror from mistake
    
    reverted change that broke tests

--- src/quest_manager/models.py
+++ src/quest_manager/models.py
@@ -158,7 +158,7 @@ class XPItem(models.Model):
     )
     archived = models.BooleanField(
         default=False,
-        help_text="Move this quest into the archived tab."
+        help_text="Move this quest into the Archived tab."
     )
     sort_order = models.IntegerField(default=0)
     max_repeats = models.IntegerField(default=0, help_text='0 = not repeatable; -1 = unlimited repeats')
@@ -514,12 +514,10 @@ class QuestManager(models.Manager):
         Returns:
             QuerySet of archived quests if user is staff, empty list otherwise
         """
-        # If the default of include_archived=False make sure this includes archived
-        qs = self.get_queryset().filter(archived=True)
         if user.is_staff:
+            qs = self.get_queryset().filter(archived=True)
             return qs
-        else:
-            return qs.none()
+        return self.get_queryset().none()
 
 
 class Quest(IsAPrereqMixin, HasPrereqsMixin, TagsModelMixin, XPItem):
