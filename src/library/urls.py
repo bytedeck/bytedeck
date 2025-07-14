6240d987e61487a3f209179252d417d1988e1b9b
thefoxwithin <marcus@walkswithin.com>
2025-07-14 10:33:51 -0700

    Views update to cbv
    urls altered to support cbv
    
    comments and docstrings added

--- src/library/urls.py
+++ src/library/urls.py
@@ -4,8 +4,8 @@ from library import views
 app_name = 'library'
 
 urlpatterns = [
-    path('quests-list/', views.quests_library_list, name='quest_list'),
-    path('campaign-list/', views.campaigns_library_list, name='category_list'),
-    path('import-campaign/<uuid:campaign_import_id>/', views.import_campaign, name='import_category'),
-    path('import-quest/<uuid:quest_import_id>/', views.import_quest_to_current_deck, name='import_quest'),
+    path('quests-list/', views.LibraryQuestListView.as_view(), name='quest_list'),
+    path('campaign-list/', views.LibraryCampaignListView.as_view(), name='category_list'),
+    path('import-campaign/<uuid:campaign_import_id>/', views.ImportCampaignView.as_view(), name='import_category'),
+    path('import-quest/<uuid:quest_import_id>/', views.ImportQuestView.as_view(), name='import_quest'),
 ]
