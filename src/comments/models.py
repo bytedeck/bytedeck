30c0fad5b3f76fe3b50b3a4f825a086e8af298d8
thefoxwithin <marcus@walkswithin.com>
2025-08-02 14:33:20 -0700

    Fix URL linkification to preserve list prefixes like 1. and handle <br>-separated input
    modified test suite
    
    removed redundant assertIns
    
    refactored test_html docstrings
    
    minor optimizations for urlize

--- src/comments/models.py
+++ src/comments/models.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.utils.html import urlize, escape
+from django.utils.html import escape
+from utilities.html import urlize
 
 from notifications.models import deleted_object_receiver
 from django.db.models.signals import pre_delete
